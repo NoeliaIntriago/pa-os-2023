282bc780a7763147b861394c00c4d66698f0aa3b
NoeliaIntriago <noinsanc@espol.edu.ec>
2023-06-09 16:08:11 -0500

    Finish timepipe.c

--- pa2-procesos-ipc/timeshmm.c
+++ pa2-procesos-ipc/timeshmm.c
@@ -8,12 +8,12 @@
 #include <sys/wait.h>
 #include <sys/time.h>
 
-#define SIZE 4096
+#define SIZE sizeof(struct timeval)
 
 int main(int argc, char *argv[]){
     if (argc < 2){
         printf("Usage: %s <command>\n", argv[0]);
-        return 1;
+        exit(1);
     }
 
     const char* name = "MAIN";  // Shared memory object name
@@ -24,27 +24,27 @@ int main(int argc, char *argv[]){
 
     shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
     if (shm_fd < 0) {
-        perror("shm_open");
-        return 1;
+        perror("ERROR: Failed to create shared memory segment (shm_open)\n");
+        exit(1);
     }
 
     ftruncate(shm_fd, SIZE);
     start_time = (struct timeval *) mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if(start_time == MAP_FAILED){
-        perror("mmap");
-        return 1;
+        perror("ERROR: Failed to attach shared memory (mmap)\n");
+        exit(1);
     }
 
     pid = fork();
     if (pid < 0) {
         // Error while creating child
-        perror("ERROR: Failed to create child process\n");
-        return 1;
+        perror("ERROR: Failed to create child process (fork)\n");
+        exit(1);
     } else if (pid == 0) {
         // Child process
         gettimeofday(start_time, NULL);
         execvp(argv[1], &argv[1]);
-        perror("ERROR: Failed to run execvp\n");
+        perror("ERROR: Failed to run execvp (execvp)\n");
         exit(status);
     } else {
         // Parent process
@@ -59,15 +59,15 @@ int main(int argc, char *argv[]){
             printf("Elapsed time: %.6f seconds\n", elapsed_time);
         } else {
             perror("ERROR: Failed to terminate child\n");
-            return 1;
+            exit(1);
         }
         if (munmap(start_time, SIZE) < 0){
-            perror("ERROR: Failed to release shared memory\n");
-            return 1;
+            perror("ERROR: Failed to release shared memory (munmap)\n");
+            exit(1);
         }
         if (shm_unlink(name)) {
-            perror("ERROR: Failed to delete shared memory\n");
-            return 1;
+            perror("ERROR: Failed to delete shared memory (shm_unlink)\n");
+            exit(1);
         }
     }
 
