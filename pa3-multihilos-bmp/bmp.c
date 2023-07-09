52cf656c30bf61cff4fd4ce34a7bae9c42955938
NoeliaIntriago <noinsanc@espol.edu.ec>
2023-07-09 11:04:25 -0500

    Changes in bmp.c, bmp.h & ex5.c

--- pa3-multihilos-bmp/bmp.c
+++ pa3-multihilos-bmp/bmp.c
@@ -1,157 +1,177 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "bmp.h"
-/* USE THIS FUNCTION TO PRINT ERROR MESSAGES
-   DO NOT MODIFY THIS FUNCTION
-*/
+
 void printError(int error) {
-  switch(error){
-  case ARGUMENT_ERROR:
-    printf("Usage:ex5 <source> <destination>\n");
-    break;
-  case FILE_ERROR:
-    printf("Unable to open file!\n");
-    break;
-  case MEMORY_ERROR:
-    printf("Unable to allocate memory!\n");
-    break;
-  case VALID_ERROR:
-    printf("BMP file not valid!\n");
-    break;
-  default:
-    break;
+  switch (error) {
+    case ARGUMENT_ERROR:
+      printf("Usage: ex5 <source> <destination>\n");
+      break;
+    case FILE_ERROR:
+      printf("Unable to open file!\n");
+      break;
+    case MEMORY_ERROR:
+      printf("Unable to allocate memory!\n");
+      break;
+    case VALID_ERROR:
+      printf("BMP file not valid!\n");
+      break;
+    default:
+      break;
   }
 }
 
-/* The input argument is the source file pointer. The function will first construct a BMP_Image image by allocating memory to it.
- * Then the function read the header from source image to the image's header.
- * Compute data size, width, height, and bytes_per_pixel of the image and stores them as image's attributes.
- * Finally, allocate menory for image's data according to the image size.
- * Return image;
-*/
 BMP_Image* createBMPImage(FILE* fptr) {
-
-  //Allocate memory for BMP_Image*;
-  BMP_Image* new_image = malloc(sizeof(BMP_Image));
-  if (new_image == NULL) {
-    printError(MEMORY_ERROR);
+  BMP_Image* image = (BMP_Image*)malloc(sizeof(BMP_Image));
+  if (image == NULL) {
     return NULL;
   }
 
-  //Read the first 54 bytes of the source into the header
-  if (fread(&(new_image->header), HEADER_SIZE, 1, fptr) != 1) {
-    printError(VALID_ERROR);
+  if (fread(&(image->header), sizeof(BMP_Header), 1, fptr) != 1) {
+    free(image);
     return NULL;
   }
 
-  //Compute data size, width, height, and bytes per pixel
-  new_image->norm_height = abs(new_image->header.height_px);
-  new_image->bytes_per_pixel = new_image->header.bits_per_pixel / 8;
-  new_image->pixels = malloc((new_image->norm_height) * sizeof(Pixel *));
+  int width_px = image->header.width_px;
+  int height_px = abs(image->header.height_px);
 
-  //Allocate memory for image data
-  for (int i = 0; i < new_image->norm_height; i++) {
-    new_image->pixels[i] = malloc((new_image->header.width_px) * sizeof(Pixel));
+  image->bytes_per_pixel = image->header.bits_per_pixel / 8;
+  image->norm_height = height_px;
+
+  image->pixels = (Pixel**)malloc(height_px * sizeof(Pixel*));
+  if (image->pixels == NULL) {
+    free(image);
+    return NULL;
   }
 
-  return new_image;
-}
-
-/* The input arguments are the source file pointer, the image data pointer, and the size of image data.
- * The functions reads data from the source into the image data matriz of pixels.
-*/
-void readImageData(FILE* srcFile, BMP_Image * image, int dataSize) {
-  for (int i = 0; i < image->norm_height; i++) {
-    for (int j = 0; j < image->header.width_px; j++) {
-      if (fread(&image->pixels[i][j], dataSize, 1, srcFile) == 0) {
-        exit(FILE_ERROR);
+  for (int i = 0; i < height_px; i++) {
+    image->pixels[i] = (Pixel*)malloc(width_px * sizeof(Pixel));
+    if (image->pixels[i] == NULL) {
+      for (int j = 0; j < i; j++) {
+        free(image->pixels[j]);
       }
+      free(image->pixels);
+      free(image);
+      return NULL;
     }
   }
-}
 
-/* The input arguments are the pointer of the binary file, and the image data pointer.
- * The functions open the source file and call to CreateBMPImage to load de data image.
-*/
-BMP_Image* readImage(FILE *srcFile) {
-  if (srcFile == NULL) {
-    printError(FILE_ERROR);
-    return NULL;
-  }
-
-  BMP_Image* image = createBMPImage(srcFile);
-  if (image == NULL) {
-    printError(MEMORY_ERROR);
-    return NULL;
-  }
-  
   return image;
 }
 
-/* The input arguments are the destination file name, and BMP_Image pointer.
- * The function write the header and image data into the destination file.
-*/
-void writeImage(char* destFileName, BMP_Image* dataImage) {
-  FILE *fd = fopen(destFileName, "wb");
-  if (fd == NULL) {
-    printError(FILE_ERROR);
-    return;
+int readImageData(FILE* srcFile, BMP_Image* image, int dataSize) {
+  for (int i = 0; i < image->norm_height; i++) {
+    for (int j = 0; j < image->header.width_px; j++) {  // Add this line
+      if (fread(&image->pixels[i][j], dataSize, 1, srcFile) != 1) {  // Modify this line
+        fprintf(stderr, "Pixel (%d,%d) could not be read from the file\n", i, j);
+        return 1;
+      }
+    }
   }
 
-  fwrite(&(dataImage->header), HEADER_SIZE, 1, fd);
-
-  for (int y = 0; y < dataImage->norm_height; y++) {
-    fwrite(dataImage->pixels[y], sizeof(Pixel), dataImage->header.width_px, fd);
-  }
-
-  fclose(fd);
-  return;
+  return 0;
 }
 
-/* The input argument is the BMP_Image pointer. The function frees memory of the BMP_Image.
-*/
+int readImage(FILE* srcFile, BMP_Image* dataImage) {
+  BMP_Image* tmp = createBMPImage(srcFile);
+  if (tmp == NULL) {
+    return 1;
+  }
+
+  *dataImage = *tmp;
+  free(tmp);
+
+  return readImageData(srcFile, dataImage, dataImage->bytes_per_pixel);
+}
+
+int writeImage(char* destFileName, BMP_Image* dataImage) {
+  FILE* output = fopen(destFileName, "wb");
+  if (fwrite(&(dataImage->header), sizeof(BMP_Header), 1, output) != 1) {
+    return 1;
+  }
+
+  for (int i = 0; i < dataImage->norm_height; i++) {
+    if (fwrite(dataImage->pixels[i], sizeof(Pixel), dataImage->header.width_px, output) != dataImage->header.width_px) {
+      fprintf(stderr, "Row (%d) could not be written to the output image\n", i);
+    }
+  }
+
+  return 0;
+}
+
+int modifyImage(BMP_Image* image, BMP_Image* new_image) {
+  memcpy(&(new_image->header), &(image->header), sizeof(BMP_Header));
+
+  switch (image->header.bits_per_pixel) {
+    case 24: {
+      new_image->header.bits_per_pixel = 32;
+      new_image->header.imagesize = image->norm_height * image->header.width_px * (new_image->header.bits_per_pixel / 8);
+      new_image->header.size = new_image->header.imagesize + sizeof(BMP_Header);
+
+      new_image->bytes_per_pixel = new_image->header.bits_per_pixel / 8;
+      new_image->norm_height = image->norm_height;
+
+      break;
+    }
+    case 32: {
+      new_image->bytes_per_pixel = image->bytes_per_pixel;
+      new_image->norm_height = image->norm_height;
+
+      break;
+    }
+  }
+
+  new_image->pixels = (Pixel**)malloc(new_image->norm_height * sizeof(Pixel*));
+  if (new_image->pixels == NULL) {
+    return 1;
+  }
+
+  for (int i = 0; i < image->norm_height; i++) {
+    new_image->pixels[i] = (Pixel*)malloc(image->header.width_px * sizeof(Pixel));
+    if (new_image->pixels[i] == NULL) {
+      for (int j = 0; j < i; j++) {
+        free(new_image->pixels[j]);
+      }
+      free(new_image->pixels);
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
 void freeImage(BMP_Image* image) {
   for (int i = 0; i < image->norm_height; i++) {
     free(image->pixels[i]);
   }
   free(image->pixels);
-
   free(image);
 }
 
-/* The functions checks if the source image has a valid format.
- * It returns TRUE if the image is valid, and returns FASLE if the image is not valid.
- * DO NOT MODIFY THIS FUNCTION
-*/
 int checkBMPValid(BMP_Header* header) {
-  // Make sure this is a BMP file
-  if (header->type != 0x4d42) {
+  if (header->type != 0x4D42) {
     return FALSE;
   }
-  // Make sure we are getting 24 bits per pixel
-  if (header->bits_per_pixel != 24) {
-    printf("We're not getting 24 bits per pixel\n");
+
+  if (!((header->bits_per_pixel & 31) == 24) && (header->bits_per_pixel != 32)) {
     return FALSE;
   }
-  // Make sure there is only one image plane
+
   if (header->planes != 1) {
-    printf("There isn't just one image\n");
     return FALSE;
   }
-  // Make sure there is no compression
+
   if (header->compression != 0) {
-    printf("There is some compression\n");
     return FALSE;
   }
+
   return TRUE;
 }
 
-/* The function prints all information of the BMP_Header.
-   DO NOT MODIFY THIS FUNCTION
-*/
 void printBMPHeader(BMP_Header* header) {
-  printf("file type (should be 0x4d42): %x\n", header->type);
+  printf("file type (should be 0x4D42): %x\n", header->type);
   printf("file size: %d\n", header->size);
   printf("offset to image data: %d\n", header->offset);
   printf("header size: %d\n", header->header_size);
@@ -161,9 +181,6 @@ void printBMPHeader(BMP_Header* header) {
   printf("bits: %d\n", header->bits_per_pixel);
 }
 
-/* The function prints information of the BMP_Image.
-   DO NOT MODIFY THIS FUNCTION
-*/
 void printBMPImage(BMP_Image* image) {
   printf("data size is %ld\n", sizeof(image->pixels));
   printf("norm_height size is %d\n", image->norm_height);
--- pa3-multihilos-bmp/ex5.c
+++ pa3-multihilos-bmp/ex5.c
@@ -5,36 +5,55 @@
 int main(int argc, char **argv) {
   FILE* source;
   FILE* dest;
-  BMP_Image* image = NULL;
-
+  BMP_Image* imageIn = NULL;
+  BMP_Image* imageOut = NULL;
+  /* Checks for 3 arguments in console */
   if (argc != 3) {
     printError(ARGUMENT_ERROR);
     exit(EXIT_FAILURE);
   }
   
+  /* Checks if source file exists */
   if((source = fopen(argv[1], "rb")) == NULL) {
     printError(FILE_ERROR);
     exit(EXIT_FAILURE);
   }
+
+  /* Checks if destiny file is valid */
   if((dest = fopen(argv[2], "wb")) == NULL) {
     printError(FILE_ERROR);
     exit(EXIT_FAILURE);
   } 
 
-  image = readImage(source);
-  printBMPHeader(&image->header);
-  if(!checkBMPValid(&image->header)) {
+  imageIn = malloc(sizeof(BMP_Image));
+  imageOut = malloc(sizeof(BMP_Image));
+  if (imageIn == NULL || imageOut == NULL) {
+    printError(MEMORY_ERROR);
+    exit(EXIT_FAILURE);
+  }
+  
+  if (readImage(source, imageIn) != 0){
+    printError(MEMORY_ERROR);
+    exit(EXIT_FAILURE);
+  }
+
+  if(!checkBMPValid(&imageIn->header)) {
     printError(VALID_ERROR);
     return EXIT_FAILURE;
   }
 
-  image = readImage(source);
-  printBMPHeader(&image->header);
-  printBMPImage(image);
+  if(modifyImage(imageIn, imageOut) != 0){
+    freeImage(imageIn);
+    freeImage(imageOut);
 
-  freeImage(image);
-  fclose(source);
-  fclose(dest);
+    fclose(source);
+    fclose(dest);
+  }
+
+  if(!checkBMPValid(&imageOut->header)) {
+    printError(VALID_ERROR);
+    exit(EXIT_FAILURE);
+  }
 
   exit(EXIT_SUCCESS);
 }
