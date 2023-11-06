fa6a9d814d362e34cf2d5293812ae662bd56ed15
Noelia Intriago <noinsanc@espol.edu.ec>
2023-11-06 16:53:49 -0500

    Changes for pa2-procesos-ipc

--- pa2-procesos-ipc/timepipe.c
+++ pa2-procesos-ipc/timepipe.c
@@ -7,47 +7,58 @@
 
 #define SIZE sizeof(struct timeval)
 
-int main(int argc, char *argv[]){
-    if (argc < 2){
-        printf("Usage: %s <command>\n", argv[0]);
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage >> %s <command>\n", argv[0]);
         return 1;
     }
 
-    int fd[2];                  // Array for pipe
+    int fd[2]; // Array for pipe
     struct timeval start_time;
     pid_t pid;
 
-    if (pipe(fd) < 0) {
-        perror("ERROR: Failed to create pipe (pipe)\n");
+    if (pipe(fd) < 0)
+    {
+        perror("ERROR >> Failed to create pipe (pipe)\n");
         exit(1);
     }
 
     pid = fork();
     int status;
-    if (pid < 0) {
+    if (pid < 0)
+    {
         // Error while creating child process
-        perror("ERROR: Failed to create child process (fork)\n");
+        perror("ERROR >> Failed to create child process (fork)\n");
         exit(1);
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         // Child process
         close(fd[0]);
         gettimeofday(&start_time, NULL);
-        if(write(fd[1], &start_time, SIZE) < 0) {
-            perror("ERROR: Failed to write to the pipe (write)\n");
+        printf("Start time: %ld seconds, %ld microseconds\n", start_time.tv_sec, start_time.tv_usec);
+        if (write(fd[1], &start_time, SIZE) < 0)
+        {
+            perror("ERROR >> Failed to write to the pipe (write)\n");
             exit(status);
         }
 
         close(fd[1]);
         execvp(argv[1], &argv[1]);
-        perror("ERROR: Failed to run execvp (execvp)\n");
+        perror("ERROR >> Failed to run execvp (execvp)\n");
         exit(status);
-    } else {
+    }
+    else
+    {
         // Parent process
         close(fd[1]);
         waitpid(pid, &status, 0);
         struct timeval start_time_buffer;
-        if (read(fd[0], &start_time_buffer, SIZE) < 0) {
-            perror("ERROR: Failed to read from the pipe (read)\n");
+        if (read(fd[0], &start_time_buffer, SIZE) < 0)
+        {
+            perror("ERROR >> Failed to read from the pipe (read)\n");
             exit(status);
         }
 
@@ -58,7 +69,8 @@ int main(int argc, char *argv[]){
         long elapsed_sec = end_time.tv_sec - start_time_buffer.tv_sec;
         long elapsed_usec = end_time.tv_usec - start_time_buffer.tv_usec;
         double elapsed_time = elapsed_sec + elapsed_usec / 1000000.0;
-        printf("Elapsed time: %.6f seconds\n", elapsed_time);
+        printf("End time: %ld seconds, %ld microseconds\n", end_time.tv_sec, end_time.tv_usec);
+        printf("Elapsed time: %f seconds\n", elapsed_time);
     }
 
     return 0;
--- pa2-procesos-ipc/timeshmm.c
+++ pa2-procesos-ipc/timeshmm.c
@@ -10,63 +10,80 @@
 
 #define SIZE sizeof(struct timeval)
 
-int main(int argc, char *argv[]){
-    if (argc < 2){
-        printf("Usage: %s <command>\n", argv[0]);
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage >> %s <command>\n", argv[0]);
         exit(1);
     }
 
-    const char* name = "MAIN";  // Shared memory object name
-    int shm_fd;                 // File descriptor for shared memory object
+    const char *name = "MAIN"; // Shared memory object name
+    int shm_fd;                // File descriptor for shared memory object
     struct timeval *start_time;
     pid_t pid;
     int status;
 
     shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-    if (shm_fd < 0) {
-        perror("ERROR: Failed to create shared memory segment (shm_open)\n");
+    if (shm_fd < 0)
+    {
+        perror("ERROR >> Failed to create shared memory segment (shm_open)\n");
         exit(1);
     }
 
     ftruncate(shm_fd, SIZE);
-    start_time = (struct timeval *) mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if(start_time == MAP_FAILED){
-        perror("ERROR: Failed to attach shared memory (mmap)\n");
+    start_time = (struct timeval *)mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (start_time == MAP_FAILED)
+    {
+        perror("ERROR >> Failed to attach shared memory (mmap)\n");
         exit(1);
     }
 
     pid = fork();
-    if (pid < 0) {
+    if (pid < 0)
+    {
         // Error while creating child
-        perror("ERROR: Failed to create child process (fork)\n");
+        perror("ERROR >> Failed to create child process (fork)\n");
         exit(1);
-    } else if (pid == 0) {
+    }
+    else if (pid == 0)
+    {
         // Child process
         gettimeofday(start_time, NULL);
+        printf("Start time: %ld seconds, %ld microseconds\n", start_time->tv_sec, start_time->tv_usec);
+
         execvp(argv[1], &argv[1]);
-        perror("ERROR: Failed to run execvp (execvp)\n");
+        perror("ERROR >> Failed to run execvp (execvp)\n");
         exit(status);
-    } else {
+    }
+    else
+    {
         // Parent process
         waitpid(pid, &status, 0);
-        if(WIFEXITED(status)) {
+        if (WIFEXITED(status))
+        {
             struct timeval end_time;
             gettimeofday(&end_time, NULL);
-            
+
             long elapsed_sec = end_time.tv_sec - start_time->tv_sec;
             long elapsed_usec = end_time.tv_usec - start_time->tv_usec;
             double elapsed_time = elapsed_sec + elapsed_usec / 1000000.0;
+            printf("End time: %ld seconds, %ld microseconds\n", end_time.tv_sec, end_time.tv_usec);
             printf("Elapsed time: %.6f seconds\n", elapsed_time);
-        } else {
-            perror("ERROR: Failed to terminate child\n");
+        }
+        else
+        {
+            perror("ERROR >> Failed to terminate child\n");
             exit(1);
         }
-        if (munmap(start_time, SIZE) < 0){
-            perror("ERROR: Failed to release shared memory (munmap)\n");
+        if (munmap(start_time, SIZE) < 0)
+        {
+            perror("ERROR >> Failed to release shared memory (munmap)\n");
             exit(1);
         }
-        if (shm_unlink(name)) {
-            perror("ERROR: Failed to delete shared memory (shm_unlink)\n");
+        if (shm_unlink(name))
+        {
+            perror("ERROR >> Failed to delete shared memory (shm_unlink)\n");
             exit(1);
         }
     }
