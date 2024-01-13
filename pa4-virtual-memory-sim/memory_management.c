2804e8dd20d5d8a9304d0d67e66a504216ecc994
Noelia Intriago <noinsanc@espol.edu.ec>
2024-01-13 15:33:40 -0500

    fix: calculations physical address and offset

--- pa4-virtual-memory-sim/memory_management.c
+++ pa4-virtual-memory-sim/memory_management.c
@@ -15,6 +15,7 @@ typedef struct
 } address;
 
 FILE *backing_store;
+int next_free_frame = 0;
 
 address get_address(unsigned int logical_address)
 {
@@ -24,26 +25,34 @@ address get_address(unsigned int logical_address)
     return address;
 }
 
-void handle_page_fault(unsigned int page_number, unsigned char *physical_memory, unsigned int *page_table)
+void handle_page_fault(unsigned int page_number, signed char *physical_memory, unsigned int *page_table)
 {
-    fseek(backing_store, page_number * PAGE_SIZE, SEEK_SET);
-    fread(&physical_memory[page_number * PAGE_SIZE], sizeof(char), PAGE_SIZE, backing_store);
-    page_table[page_number] = page_number * PAGE_SIZE;
-}
-
-void read_backing_store(unsigned char *physical_memory, unsigned int *page_table)
-{
-    for (unsigned int i = 0; i < PHYSICAL_MEM_SIZE; ++i)
+    if (next_free_frame >= NUM_PAGES)
     {
-        physical_memory[i] = 0;
+        printf("Error: Out of memory\n");
+        exit(EXIT_FAILURE);
     }
 
-    for (unsigned int i = 0; i < NUM_PAGES; ++i)
+    fseek(backing_store, page_number * PAGE_SIZE, SEEK_SET);
+    fread(&physical_memory[next_free_frame * PAGE_SIZE], sizeof(char), FRAME_SIZE, backing_store);
+    page_table[page_number] = next_free_frame;
+    next_free_frame++;
+}
+
+void read_backing_store(signed char *physical_memory, unsigned int *page_table)
+{
+    memset(physical_memory, 0, PHYSICAL_MEM_SIZE);
+    for (unsigned int i = 0; i < NUM_PAGES; i++)
     {
         page_table[i] = -1;
     }
 
     backing_store = fopen("data/BACKING_STORE.bin", "rb");
+    if (backing_store == NULL)
+    {
+        printf("Error opening backing store\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char *argv[])
@@ -63,7 +72,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    unsigned char physical_memory[PHYSICAL_MEM_SIZE];
+    signed char physical_memory[PHYSICAL_MEM_SIZE];
     unsigned int page_table[NUM_PAGES];
 
     read_backing_store(physical_memory, page_table);
@@ -78,7 +87,8 @@ int main(int argc, char *argv[])
             handle_page_fault(addr.page_number, physical_memory, page_table);
         }
 
-        unsigned int physical_address = page_table[addr.page_number] + addr.offset;
+        unsigned int frame_number = page_table[addr.page_number];
+        unsigned int physical_address = frame_number * FRAME_SIZE + addr.offset;
         unsigned int value = physical_memory[physical_address];
         fprintf(output_file, "Virtual address: %d Physical address: %d Value: %d\n", logical_address, physical_address, value);
     }
